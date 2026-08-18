fb148a5f83fd6f04deacef95e7dd5a8ed3b3cde4
ben-at-c2k <benjamin@core2000.com>
2026-08-18 23:45:01 +0500

    Create /LICENSE and /setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#Copyright (c) 2026 Benjamin Winter
+#This file is part of BasicMemManip which is released under the MIT License.
+#See file LICENSE.TXT or go to https://github.com/core2000-eU/BasicMemManip for full license details.
+
+#DESCRIPTION
+#setup.py for BasicMemManip, a Python module that extends Python with some low-level, C/C++ -related features (intended to be used in combination with Pythons ctypes library)
+
+import                          setuptools
+
+setuptools.setup (
+    name =                      "BasicMemManip", 
+    version =                   '0.0.1',
+    author =                    "Benjamin Winter",
+    license=                    "MIT",
+    description =               "BasicMemManip, a Python module that extends Python with some low-level, C/C++ -related features (intended to be used in combination with Pythons ctypes library)",
+    long_description =          "BasicMemManip, a Python module that extends Python with some low-level, C/C++ -related features (intended to be used in combination with Pythons ctypes library)",
+    keywords =                  ['C', 'C++', 'low-level', 'ctypes', 'WSTR', 'memory'],
+    classifiers =               [
+                                    "Development Status :: 4 - Beta",
+                                    "Programming Language :: Python :: 3",
+                                    "Operating System :: OS Independent"
+                                ],
+    packages =                  setuptools.find_namespace_packages(where="src/P"),
+    package_dir =               {"": "src/P"},
+    install_requires =          [],
+    package_data =              {},
+)
