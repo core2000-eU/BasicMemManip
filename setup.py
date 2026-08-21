b67286e8f9ee143a56d1c2f4e3a3bf1e8270c50f
ben-at-c2k <benjamin@core2000.com>
2026-08-21 13:15:55 +0500

    Update setup.py
    added reference to README.md

--- setup.py
+++ setup.py
@@ -6,22 +6,27 @@
 #setup.py for BasicMemManip, a Python module that extends Python with some low-level, C/C++ -related features (intended to be used in combination with Pythons ctypes library)
 
 import                          setuptools
+from pathlib import             Path
+
+parent_dir = Path(__file__).parent
+long_description = (parent_dir / "README.md").read_text(encoding="utf-8")
 
 setuptools.setup (
-    name =                      "BasicMemManip", 
-    version =                   '0.0.1',
-    author =                    "Benjamin Winter",
-    license=                    "MIT",
-    description =               "BasicMemManip, a Python module that extends Python with some low-level, C/C++ -related features (intended to be used in combination with Pythons ctypes library)",
-    long_description =          "BasicMemManip, a Python module that extends Python with some low-level, C/C++ -related features (intended to be used in combination with Pythons ctypes library)",
-    keywords =                  ['C', 'C++', 'low-level', 'ctypes', 'WSTR', 'memory'],
-    classifiers =               [
-                                    "Development Status :: 4 - Beta",
-                                    "Programming Language :: Python :: 3",
-                                    "Operating System :: OS Independent"
-                                ],
-    packages =                  setuptools.find_namespace_packages(where="src/P"),
-    package_dir =               {"": "src/P"},
-    install_requires =          [],
-    package_data =              {},
+    name =                              "BasicMemManip", 
+    version =                           '0.0.1',
+    author =                            "Benjamin Winter",
+    license=                            "MIT",
+    description =                       "BasicMemManip, a Python module that extends Python with some low-level, C/C++ -related features (intended to be used in combination with Pythons ctypes library)",
+    long_description =                  long_description,
+    long_description_content_type =     "text/markdown",
+    keywords =                          ['C', 'C++', 'low-level', 'ctypes', 'WSTR', 'memory'],
+    classifiers =                       [
+                                            "Development Status :: 4 - Beta",
+                                            "Programming Language :: Python :: 3",
+                                            "Operating System :: OS Independent"
+                                        ],
+    packages =                          setuptools.find_namespace_packages(where="src/P"),
+    package_dir =                       {"": "src/P"},
+    install_requires =                  [],
+    package_data =                      {},
 )
